a65f3daf6536402c799d28ccae12b43affbb87a4
GoggledPetilil <anakcebu@gmail.com>
2026-03-13 17:02:24 +0100

    Stat boosts display
    - Optimized the stat boost display script

--- Plugins/EBDX Stat Boost Overlay/Script.rb
+++ Plugins/EBDX Stat Boost Overlay/Script.rb
@@ -2,180 +2,80 @@ class DataBoxEBDX  <  SpriteWrapper
   alias setUp_StatOverlay setUp
   def setUp
     setUp_StatOverlay
-    sXp = 176
-    sYp = @battle.singleBattle? ? -46 : -34
-    sXe = 4
-    sYe = @battle.singleBattle? ? 22 : 18
-    sXoffset = 46
-    sYoffset = 22
+    @showBoosts = true                          # No real reason to turn this off, unless you wanna tie this to a switch or smth lol
+    @singleOnly = false                         # Only display stat boosts during single battles
+    doublesSingleLine = true                    # Display stat boosts on a single line instead during double battles
 
-    if @battle.singleBattle?
-      @sprites["boost1"] = Sprite.new(@viewport)
-      @sprites["boost1"].bitmap = nil
-      @sprites["boost1"].z = 1
-      @sprites["boost1"].ex = @playerpoke ? (sXp - sXoffset * 0) : (sXe + sXoffset * 0)
-      @sprites["boost1"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost1"].visible = false
+    sXp = @battle.singleBattle? ? 172 : 172     # The horizontal position of the FIRST stat change on the Player's PKMN
+    sYp = @battle.singleBattle? ? -46 : -36     # The vertical position of the FIRST stat change on the Player's PKMN
+    sXe = @battle.singleBattle? ? 4 : 4         # The horizontal position of the FIRST stat change on the Opponent's PKMN
+    sYe = @battle.singleBattle? ? 22 : 14       # The vertical position of the FIRST stat change on the Opponent's PKMN
+    sXoffset = 46                               # The amount of horizontal space between stat changes
+    sYoffset = 22                               # The amount of vertical space between stat changes
+    statswidth = 4                              # The amount of stat changes displayed per line
 
-      @sprites["boost2"] = Sprite.new(@viewport)
-      @sprites["boost2"].bitmap = nil
-      @sprites["boost2"].z = 1
-      @sprites["boost2"].ex = @playerpoke ? (sXp - sXoffset * 1) : (sXe + sXoffset * 1)
-      @sprites["boost2"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost2"].visible = false
+    @statAmount = 7                             # How many stats to display. Unless you know what you're doing, don't change this.
 
-      @sprites["boost3"] = Sprite.new(@viewport)
-      @sprites["boost3"].bitmap = nil
-      @sprites["boost3"].z = 1
-      @sprites["boost3"].ex = @playerpoke ? (sXp - sXoffset * 2) : (sXe + sXoffset * 2)
-      @sprites["boost3"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost3"].visible = false
+    # Create the sprites (not visible yet)
+    @statAmount.times do |i|
+      bX = @playerpoke ? sXp : sXe
+      bY = @playerpoke ? sYp : sYe
+      # Ensure the player's boosts are right-aligned going up, while opp's boosts are left-aligned going down
+      offsetX = @playerpoke ? sXoffset : (sXoffset * -1)
+      offsetY = @playerpoke ? sYoffset : (sYoffset * -1)
+      stackBoosts = @battle.singleBattle? || doublesSingleLine == false
+      xOffsetPower = stackBoosts ? i % statswidth : i
+      yOffsetPower = stackBoosts ? (i / statswidth).floor : 0
 
-      @sprites["boost4"] = Sprite.new(@viewport)
-      @sprites["boost4"].bitmap = nil
-      @sprites["boost4"].z = 1
-      @sprites["boost4"].ex = @playerpoke ? (sXp - sXoffset * 3) : (sXe + sXoffset * 3)
-      @sprites["boost4"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost4"].visible = false
-
-      @sprites["boost5"] = Sprite.new(@viewport)
-      @sprites["boost5"].bitmap = nil
-      @sprites["boost5"].z = 1
-      @sprites["boost5"].ex = @playerpoke ? (sXp - sXoffset * 0) : (sXe + sXoffset * 0)
-      @sprites["boost5"].ey = @playerpoke ? (sYp - sYoffset * 1) : (sYe + sYoffset * 1)
-      @sprites["boost5"].visible = false
-
-      @sprites["boost6"] = Sprite.new(@viewport)
-      @sprites["boost6"].bitmap = nil
-      @sprites["boost6"].z = 1
-      @sprites["boost6"].ex = @playerpoke ? (sXp - sXoffset * 1) : (sXe + sXoffset * 1)
-      @sprites["boost6"].ey = @playerpoke ? (sYp - sYoffset * 1) : (sYe + sYoffset * 1)
-      @sprites["boost6"].visible = false
-
-      @sprites["boost7"] = Sprite.new(@viewport)
-      @sprites["boost7"].bitmap = nil
-      @sprites["boost7"].z = 1
-      @sprites["boost7"].ex = @playerpoke ? (sXp - sXoffset * 2) : (sXe + sXoffset * 2)
-      @sprites["boost7"].ey = @playerpoke ? (sYp - sYoffset * 1) : (sYe + sYoffset * 1)
-      @sprites["boost7"].visible = false
-    else
-      @sprites["boost1"] = Sprite.new(@viewport)
-      @sprites["boost1"].bitmap = nil
-      @sprites["boost1"].z = 1
-      @sprites["boost1"].ex = @playerpoke ? (sXp - sXoffset * 0) : (sXe + sXoffset * 0)
-      @sprites["boost1"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost1"].visible = false
-
-      @sprites["boost2"] = Sprite.new(@viewport)
-      @sprites["boost2"].bitmap = nil
-      @sprites["boost2"].z = 1
-      @sprites["boost2"].ex = @playerpoke ? (sXp - sXoffset * 1) : (sXe + sXoffset * 1)
-      @sprites["boost2"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost2"].visible = false
-
-      @sprites["boost3"] = Sprite.new(@viewport)
-      @sprites["boost3"].bitmap = nil
-      @sprites["boost3"].z = 1
-      @sprites["boost3"].ex = @playerpoke ? (sXp - sXoffset * 2) : (sXe + sXoffset * 2)
-      @sprites["boost3"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost3"].visible = false
-
-      @sprites["boost4"] = Sprite.new(@viewport)
-      @sprites["boost4"].bitmap = nil
-      @sprites["boost4"].z = 1
-      @sprites["boost4"].ex = @playerpoke ? (sXp - sXoffset * 3) : (sXe + sXoffset * 3)
-      @sprites["boost4"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost4"].visible = false
-
-      @sprites["boost5"] = Sprite.new(@viewport)
-      @sprites["boost5"].bitmap = nil
-      @sprites["boost5"].z = 1
-      @sprites["boost5"].ex = @playerpoke ? (sXp - sXoffset * 4) : (sXe + sXoffset * 4)
-      @sprites["boost5"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost5"].visible = false
-
-      @sprites["boost6"] = Sprite.new(@viewport)
-      @sprites["boost6"].bitmap = nil
-      @sprites["boost6"].z = 1
-      @sprites["boost6"].ex = @playerpoke ? (sXp - sXoffset * 5) : (sXe + sXoffset * 5)
-      @sprites["boost6"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost6"].visible = false
-
-      @sprites["boost7"] = Sprite.new(@viewport)
-      @sprites["boost7"].bitmap = nil
-      @sprites["boost7"].z = 1
-      @sprites["boost7"].ex = @playerpoke ? (sXp - sXoffset * 6) : (sXe + sXoffset * 6)
-      @sprites["boost7"].ey = @playerpoke ? (sYp - sYoffset * 0) : (sYe + sYoffset * 0)
-      @sprites["boost7"].visible = false
+      @sprites["boost#{i+1}"] = Sprite.new(@viewport)
+      @sprites["boost#{i+1}"].bitmap = nil
+      @sprites["boost#{i+1}"].z = 1
+      @sprites["boost#{i+1}"].ex = (bX - offsetX * xOffsetPower)
+      @sprites["boost#{i+1}"].ey = (bY - offsetY * yOffsetPower)
+      @sprites["boost#{i+1}"].visible = false
     end
   end
   alias update_StatOverlay update
   def update
     update_StatOverlay
-    # shows stat boosts
+    # Get all stat boosts
+    stats = [:ATTACK,:DEFENSE,:SPECIAL_ATTACK,:SPECIAL_DEFENSE,:SPEED,:ACCURACY,:EVASION]
     stat_boost = []
-    $stat_boost = stat_boost
-    bAtk = @battler.stages[:ATTACK]
-    bDef = @battler.stages[:DEFENSE]
-    bSAt = @battler.stages[:SPECIAL_ATTACK]
-    bSDf = @battler.stages[:SPECIAL_DEFENSE]
-    bSpd = @battler.stages[:SPEED]
-    bAcc = @battler.stages[:ACCURACY]
-    bEva = @battler.stages[:EVASION]
-    stat_boost.push(:ATTACK) 		if bAtk != 0
-    stat_boost.push(:DEFENSE) 		if bDef != 0
-    stat_boost.push(:SPECIAL_ATTACK) 	if bSAt != 0
-    stat_boost.push(:SPECIAL_DEFENSE) 	if bSDf != 0
-    stat_boost.push(:SPEED) 		if bSpd != 0
-    stat_boost.push(:ACCURACY) 		if bAcc != 0
-    stat_boost.push(:EVASION) 		if bEva != 0
+    @statAmount.times do |i|
+      stat = stats[i.clamp(0,stats.length)] # Fail-safe
+      stat_boost.push(stat) if @battler.stages[stat] != 0
+    end
 
-    @sprites["boost1"].bitmap = (stat_boost.size > 0) ? pbBitmap(getBitmap(stat_boost[0])) : nil
-    @sprites["boost1"].visible = (stat_boost.size > 0 && self.visible == true) ? true : false
+    # Assign correct sprite according to stat boosts
+    @statAmount.times do |i|
+      sprite = @sprites["boost#{i+1}"]
+      showSprites = (@battle.singleBattle? || !@singleOnly) && @showBoosts
+      if stat_boost.size > i && showSprites && self.visible # Makes the stat changes disappear if HP bar disappears lol
+        stat = stat_boost[i]
 
-    @sprites["boost2"].bitmap = (stat_boost.size > 1) ? pbBitmap(getBitmap(stat_boost[1])) : nil
-    @sprites["boost2"].visible = (stat_boost.size > 1 && self.visible == true) ? true : false
-
-    @sprites["boost3"].bitmap = (stat_boost.size > 2) ? pbBitmap(getBitmap(stat_boost[2])) : nil
-    @sprites["boost3"].visible = (stat_boost.size > 2 && self.visible == true) ? true : false
-
-    @sprites["boost4"].bitmap = (stat_boost.size > 3) ? pbBitmap(getBitmap(stat_boost[3])) : nil
-    @sprites["boost4"].visible = (stat_boost.size > 3 && self.visible == true) ? true : false
-
-    @sprites["boost5"].bitmap = (stat_boost.size > 4) ? pbBitmap(getBitmap(stat_boost[4])) : nil
-    @sprites["boost5"].visible = (stat_boost.size > 4 && self.visible == true) ? true : false
-
-    @sprites["boost6"].bitmap = (stat_boost.size > 5) ? pbBitmap(getBitmap(stat_boost[5])) : nil
-    @sprites["boost6"].visible = (stat_boost.size > 5 && self.visible == true) ? true : false
-
-    @sprites["boost7"].bitmap = (stat_boost.size > 6) ? pbBitmap(getBitmap(stat_boost[6])) : nil
-    @sprites["boost7"].visible = (stat_boost.size > 6 && self.visible == true) ? true : false
+        sprite.bitmap = pbBitmap(getBitmap(stat))
+        getRect(sprite, stat)
+        sprite.visible = true 
+      else
+        sprite.bitmap = nil
+        sprite.visible = false
+      end
+    end
   end
 
   def getBitmap(stat)
-    string = @path + "Modifiers/"
-    bAtk = @battler.stages[:ATTACK]
-    bDef = @battler.stages[:DEFENSE]
-    bSAt = @battler.stages[:SPECIAL_ATTACK]
-    bSDf = @battler.stages[:SPECIAL_DEFENSE]
-    bSpd = @battler.stages[:SPEED]
-    bAcc = @battler.stages[:ACCURACY]
-    bEva = @battler.stages[:EVASION]
-    if stat.to_s == "ATTACK"
-      string = string + "Atk#{bAtk}"
-    elsif stat.to_s == "DEFENSE"
-      string = string + "Def#{bDef}"
-    elsif stat.to_s == "SPECIAL_ATTACK"
-      string = string + "SpAtk#{bSAt}"
-    elsif stat.to_s == "SPECIAL_DEFENSE"
-      string = string + "SpDef#{bSDf}"
-    elsif stat.to_s == "SPEED"
-      string = string + "Spe#{bSpd}"
-    elsif stat.to_s == "ACCURACY"
-      string = string + "Acc#{bAcc}"
-    elsif stat.to_s == "EVASION"
-      string = string + "Eva#{bEva}"
-    end
+    s = GameData::Stat.get(stat)
+    string = @path + "Modifiers/changes_#{s.name_brief}"
     return string
+  end
+
+  def getRect(sprite, stat)
+    iconWidth = 48
+    iconHeight = 26
+    stage = @battler.stages[stat]
+    x = iconWidth * (stage > 0 ? 0 : 1)
+    y = iconHeight * (stage.abs - 1)
+
+    sprite.src_rect.set(x, y, iconWidth, iconHeight)
   end
 end
